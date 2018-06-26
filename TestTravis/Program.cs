9a2b938e4a83e567c9c21c279dc9bc05822fc27b
metagrobil <metagrobil@gmail.com>
2018-06-26 09:55:45 +0200

    xxqxxq

--- TestTravis/Program.cs
+++ TestTravis/Program.cs
@@ -10,7 +10,6 @@ namespace TestTravis
     {
         static void Main(string[] args)
         {
-            Class1 class1 = new Class1();
             if (args.Length == 3)
                 Console.WriteLine("Mes couilles en string");
                 return;
