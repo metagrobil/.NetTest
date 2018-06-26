9c324aca29a7cefa45a997acd5e4b9f6531464f8
metagrobil <metagrobil@gmail.com>
2018-06-26 08:48:36 +0200

    class1

---
+++ TestTravis/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestTravis
+{
+    class Class1
+    {
+        int oui;
+        public Class1()
+        {
+
+        }
+
+        private void koffkdsnjkfsn(string useless)
+        {
+            return;
+        }
+
+    }
+}
