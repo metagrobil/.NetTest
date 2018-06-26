867fb5a20d1a1e8981de16d9cf8e43dcc94ac7f1
metagrobil <metagrobil@gmail.com>
2018-06-26 10:36:55 +0200

    dsdsqdsqqddq

---
+++ TestTravis/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestTravis
+{
+    public class Class1
+    {
+        int i;
+        public Class1()
+        {
+            Console.Write("Class1");
+        }
+    }
+}
