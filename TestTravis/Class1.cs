574ba9d618eb19a558f37acd8af61bbadfdaaa16
metagrobil <metagrobil@gmail.com>
2018-06-26 08:53:06 +0200

    dsqdds

--- TestTravis/Class1.cs
+++ TestTravis/Class1.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace TestTravis
 {
-    class Class1
+    public class Class1
     {
         int oui;
         public Class1()
--- TestTravis/Program.cs
+++ TestTravis/Program.cs
@@ -10,6 +10,7 @@ namespace TestTravis
     {
         static void Main(string[] args)
         {
+            Class1 class1 = new Class1();
             if (args.Length == 3)
                 Console.WriteLine("Mes couilles en string");
                 return;
